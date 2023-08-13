a734fa07907f46d0192a2e7dba55ccb609190356
Stan-breaks <108292766+Stan-breaks@users.noreply.github.com>
2023-08-13 11:08:22 +0300

    timezone fix

--- todolist/settings.py
+++ todolist/settings.py
@@ -106,11 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
